85cf3fbcf7e1d830643ab836e2e5e51db4b495ba
Bin Liu <biliu@vmware.com>
2023-04-14 02:18:22 +0800

    test

---
+++ apis/addonconfigs/cni/v1alpha1/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2022 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// Package v1alpha1 contains the v1alpha1 API implementation.
+// +groupName=cni.tanzu.vmware.com
+// +k8s:deepcopy-gen=package
+// +k8s:conversion-gen=github.com/vmware-tanzu/tanzu-framework/apis/addonconfigs/cni/v1alpha2
+package v1alpha1
---
+++ apis/addonconfigs/cni/v1alpha2/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2022 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// Package v1alpha2 contains the v1alpha1 API implementation.
+// +groupName=cni.tanzu.vmware.com
+// +k8s:deepcopy-gen=package
+// +k8s:conversion-gen=github.com/vmware-tanzu/tanzu-framework/apis/addonconfigs/cni/v1alpha1
+package v1alpha2
